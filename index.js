3bbf8762537e003d0c1b89031ec3c6f82f0cffd0
StephJaNa <160164819+StephJaNa@users.noreply.github.com>
2024-04-21 14:15:20 -0600

    Index con métodos
    Salida de métodos de texto plano y de objetos de JSON de números aleatorios

--- index.js
+++ index.js
@@ -25,60 +25,38 @@ app.get('/', (req, res) => {
 // POST en la ruta raíz ('/')
 app.post('/', (req, res) => {
     username = 'Hola, soy el método POST'
-    res.json(username);
+    res.send(username);
 });
 
 // PUT en la ruta raíz ('/')
 app.put('/', (req, res) => {
     username = 'Hola, soy el método PUT'
-    res.json(username);
+    res.send(username);
 });
+
+const Aleatorio = () => Math.floor(Math.random() * 1000) + 1;
 
 // PATCH en la ruta raíz ('/')
 app.patch('/', (req, res) => {
-    username = 'Hola, soy el método PATCH'
-    res.json(username);
+    const num = Aleatorio();
+    salida = {
+        metodo: "Hola, soy el método PATCH",
+        numero_aleatorio: num
+    }
+    res.json(salida);
 });
 
 // DELETE en la ruta raíz ('/')
 app.delete('/', (req, res) => {
-    username = 'Hola, soy el método DELETE'
-    res.json(username);
+    const num = Aleatorio();
+    salida = {
+        metodo: "Hola, soy el método DELETE",
+        numero_aleatorio: num
+    }
+    res.json(salida);
 });
 
-// OPTIONS en la ruta raíz ('/')
-app.options('/', (req, res) => {
-    username = 'Hola, soy el método OPTIONS'
-    res.json(username);
-});
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
-
-
-
-//const express = require('express')
-//const app = express()
-//const port = 3000
-//var cors = require('cors')
-//app.use(cors())
-//app.use(express.urlencoded());
-//app.use(express.json());
-
-//app.listen(port,() => {
-//    console.log(`Example app listening on port ${port}`)
-//})
-
-
-
-
-//console.log('hola mundo');
-//console.log('hola mundo');
-//console.log('hola mundo');
-//console.log("Adios");
-
-//function hola(mensaje) {
-  //  return `Hola ${mensaje}`;
-//}
-//console.log(hola('mundo'));
